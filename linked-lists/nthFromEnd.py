5dd9e3ac492ce04e3f7646a87ace2f8a4993e83a
Taylor Parrish <twpearish@gmail.com>
2018-06-11 21:26:21 +0200

    added iterative solution

--- linked-lists/nthFromEnd.py
+++ linked-lists/nthFromEnd.py
@@ -39,3 +39,31 @@ class Solution:
         
         # return the head
         return head
+
+
+# Solution #2 beats 55%
+# Time : O(n) Space : O(1)
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast = slow = head
+        
+        for i in range(n):
+            #[slow] -> [n1] -> [fast]
+            fast = fast.next
+            
+        if not fast:
+            return head.next
+            
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+            
+        slow.next = slow.next.next
+        
+        return head
